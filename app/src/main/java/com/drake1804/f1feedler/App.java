44d622b114f582d76bd3686715b1efb281a36eea
Pavel.Shkaran <drake1804@gmail.com>
2016-06-01 17:14:32 +0300

    ---

--- app/src/main/java/com/drake1804/f1feedler/App.java
+++ app/src/main/java/com/drake1804/f1feedler/App.java
@@ -3,7 +3,11 @@ package com.drake1804.f1feedler;
 import android.app.Application;
 
 import com.drake1804.f1feedler.utils.DataSourceController;
+import com.facebook.stetho.Stetho;
 import com.squareup.picasso.Picasso;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import java.util.regex.Pattern;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -27,6 +31,21 @@ public class App extends Application {
 
 //            Picasso.with(getApplicationContext())
 //                    .setLoggingEnabled(true);
+
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                            .build());
+
+            RealmInspectorModulesProvider.builder(this)
+                    .withFolder(getCacheDir())
+                    .withEncryptionKey("encrypted.realm", "keyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy".getBytes())
+                    .withMetaTables()
+                    .withDescendingOrder()
+                    .withLimit(1000)
+                    .databaseNamePattern(Pattern.compile(".+\\.realm"))
+                    .build();
         }
     }
 }
